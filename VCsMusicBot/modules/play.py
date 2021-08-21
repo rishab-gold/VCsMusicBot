01bfdc62e7e54e940d2a315f8d0c3c54f7e65a02
rishab-gold <87978552+rishab-gold@users.noreply.github.com>
2021-08-21 19:21:50 +0530

    Update play.py

--- VCsMusicBot/modules/play.py
+++ VCsMusicBot/modules/play.py
@@ -534,7 +534,7 @@ async def play(_, message: Message):
         )
         file_name = get_file_name(audio)
         title = file_name
-        thumb_name = "https://telegra.ph/file/cf19dda907391656338eb.png"
+        thumb_name = "https://telegra.ph/file/01044adf16201d3226491.jpg"
         thumbnail = thumb_name
         duration = round(audio.duration / 60)
         views = "Locally added"
@@ -961,7 +961,7 @@ async def deezer(client: Client, message_: Message):
         url = songs.result[0].url
         artist = songs.result[0].artist
         duration = songs.result[0].duration
-        thumbnail = "https://telegra.ph/file/cf19dda907391656338eb.png"
+        thumbnail = "https://telegra.ph/file/01044adf16201d3226491.jpg"
 
     except:
         await res.edit("Found Literally Nothing, You Should Work On Your English!")
