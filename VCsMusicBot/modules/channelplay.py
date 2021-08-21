3e48c714e1dc25bf25e02116907b973067a06fca
rishab-gold <87978552+rishab-gold@users.noreply.github.com>
2021-08-21 19:20:14 +0530

    Update channelplay.py

--- VCsMusicBot/modules/channelplay.py
+++ VCsMusicBot/modules/channelplay.py
@@ -453,7 +453,7 @@ async def play(_, message: Message):
         )
         file_name = get_file_name(audio)
         title = file_name
-        thumb_name = "https://telegra.ph/file/f6086f8909fbfeb0844f2.png"
+        thumb_name = "https://telegra.ph/file/01044adf16201d3226491.jpg"
         thumbnail = thumb_name
         duration = round(audio.duration / 60)
         views = "Locally added"
